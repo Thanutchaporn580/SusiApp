2e3c1da46fa366d82a094cee6f3cfacc5e66b03b
Thanutchaporn Boonyavong <tgfonefone2559@gmail.com>
2025-01-24 09:30:20 +0700

    Firstly, I import Popup. After that, I add function that set turn on or turn off of sound and music but I didn't put the music yet. Creat many buttons of setting and set default value.

--- hit_the_seal.py
+++ hit_the_seal.py
@@ -6,6 +6,7 @@ from kivy.uix.label import Label
 from kivy.uix.floatlayout import FloatLayout
 from kivy.core.audio import SoundLoader
 from kivy.uix.button import Button
+from kivy.uix.popup import Popup
 
 class MainScreen(Screen):
     pass
@@ -71,6 +72,11 @@ class CustomButton(Button):
         return super().on_press()
 
 class HitTheSealApp(App):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.sound_enabled = True  # Set default value
+        self.effects_enabled = True  
+
     def build(self):
         Builder.load_file('hit_the_seal.kv')
         sm = ScreenManager()
@@ -80,6 +86,61 @@ class HitTheSealApp(App):
         sm.add_widget(GameScreen(name='game'))
         sm.add_widget(EndScreen(name='end'))
         return sm
+    
+    def toggle_sound(self):
+        self.sound_enabled = not self.sound_enabled
+        print(f"Sound enabled: {self.sound_enabled}")
+
+    def toggle_effects(self):
+        self.effects_enabled = not self.effects_enabled
+        print(f"Effects enabled: {self.effects_enabled}")
+
+    def show_settings_popup(self):
+        popup_content = FloatLayout()
+        sound_label = Label(
+            text="Sound",
+            size_hint=(0.4, 0.1),
+            pos_hint={"x": 0.1, "y": 0.6},
+            font_size=20,
+        )
+        effects_label = Label(
+            text="Music",
+            size_hint=(0.4, 0.1),
+            pos_hint={"x": 0.1, "y": 0.4},
+            font_size=20,
+        )
+        sound_toggle = Button(
+            text="ON" if self.sound_enabled else "OFF",
+            size_hint=(0.2, 0.1),
+            pos_hint={"x": 0.7, "y": 0.6},
+            on_press=lambda x: self.toggle_sound(),
+        )
+        effects_toggle = Button(
+            text="ON" if self.effects_enabled else "OFF",
+            size_hint=(0.2, 0.1),
+            pos_hint={"x": 0.7, "y": 0.4},
+            on_press=lambda x: self.toggle_effects(),
+        )
+        close_button = Button(
+            text="Close",
+            size_hint=(0.4, 0.1),
+            pos_hint={"center_x": 0.5, "y": 0.1},
+            on_press=lambda x: settings_popup.dismiss(),
+        )
+
+        popup_content.add_widget(sound_label)
+        popup_content.add_widget(effects_label)
+        popup_content.add_widget(sound_toggle)
+        popup_content.add_widget(effects_toggle)
+        popup_content.add_widget(close_button)
+
+        settings_popup = Popup(
+            title="Setting",
+            content=popup_content,
+            size_hint=(0.8, 0.5),
+            auto_dismiss=False,
+        )
+        settings_popup.open()
     
     def play_again(self):
         #add delay 0.5 sec before change screeen
